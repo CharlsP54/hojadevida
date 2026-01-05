e1574da107c1a75df2f2299c125d02a55fc4d9a7
CharlsP54 <carlosponce54104@hotmail.com>
2026-01-04 21:22:43 -0500

    5535251ssaw

--- startup.sh
+++ startup.sh
@@ -3,14 +3,19 @@ set -e
 
 cd /home/site/wwwroot
 
-echo "==> Python version"
+echo "==> Python:"
 python --version
+echo "==> Pip:"
+python -m pip --version
 
 echo "==> Upgrade pip"
 python -m pip install --upgrade pip
 
 echo "==> Install requirements"
-pip install -r requirements.txt
+python -m pip install -r requirements.txt
+
+echo "==> Check Django"
+python -c "import django; print('Django OK:', django.get_version())"
 
 echo "==> Migrate"
 python manage.py migrate --noinput
@@ -23,4 +28,3 @@ exec gunicorn hojadevida.wsgi:application \
   --bind 0.0.0.0:${PORT:-8000} \
   --workers 2 \
   --timeout 120
-22
--- startup_wrapper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-chmod +x /home/site/wwwroot/startup.sh || true
-bash /home/site/wwwroot/startup.sh
