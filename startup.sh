9189263f756dcbf9284c76353631947714bec3bb
CharlsP54 <carlosponce54104@hotmail.com>
2026-01-04 21:19:16 -0500

    2256361115

--- startup.sh
+++ startup.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 set -e
 
-echo "==> Ir a wwwroot"
 cd /home/site/wwwroot
 
-echo "==> Python & pip"
+echo "==> Python version"
 python --version
-python -m pip --version
 
 echo "==> Upgrade pip"
 python -m pip install --upgrade pip
 
-echo "==> Instalar dependencias"
-python -m pip install -r requirements.txt
+echo "==> Install requirements"
+pip install -r requirements.txt
 
-echo "==> Migraciones"
+echo "==> Migrate"
 python manage.py migrate --noinput
 
 echo "==> Collectstatic"
 python manage.py collectstatic --noinput
 
-echo "==> Arrancar Gunicorn"
+echo "==> Start gunicorn"
 exec gunicorn hojadevida.wsgi:application \
   --bind 0.0.0.0:${PORT:-8000} \
   --workers 2 \
   --timeout 120
+22
