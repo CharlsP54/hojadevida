ba6cdad5504734e0719a84a9670aabea476d7720
CharlsP54 <carlosponce54104@hotmail.com>
2026-01-04 20:43:09 -0500

    gfgdgdvfdvd

--- startup.sh
+++ startup.sh
@@ -3,10 +3,11 @@ set -e
 
 cd /home/site/wwwroot
 
-echo "==> Arrancando Gunicorn"
+# (Opcional) si manage.py NO está aquí, usa:
+# cd /home/site/wwwroot/hojadevida
+
+# Arranque directo (rápido)
 exec gunicorn hojadevida.wsgi:application \
   --bind 0.0.0.0:${PORT:-8000} \
   --workers 2 \
-  --timeout 120 \
-  --access-logfile - \
-  --error-logfile -
+  --timeout 120
