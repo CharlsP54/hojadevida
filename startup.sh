3761ac246ab2648fb7b78753acb8589c62ddfe88
CharlsP54 <carlosponce54104@hotmail.com>
2026-01-04 20:34:25 -0500

    skhadida

--- startup.sh
+++ startup.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "==> Iniciando en: $(pwd)"
-echo "==> Listando archivos en /home/site/wwwroot"
 cd /home/site/wwwroot
-ls -la
 
-# (Opcional) si necesitas ver si existe manage.py:
-test -f manage.py && echo "✅ manage.py encontrado" || (echo "❌ No encuentro manage.py" && exit 1)
-
-echo "==> Arrancando Gunicorn (rápido)"
+echo "==> Arrancando Gunicorn"
 exec gunicorn hojadevida.wsgi:application \
   --bind 0.0.0.0:${PORT:-8000} \
   --workers 2 \
-  --timeout 120
+  --timeout 120 \
+  --access-logfile - \
+  --error-logfile -
