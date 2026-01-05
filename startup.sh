bcb1e43e2fb7e1ed508884c448d919c5e3996c9e
CharlsP54 <carlosponce54104@hotmail.com>
2026-01-04 19:53:46 -0500

    Con fe

--- startup.sh
+++ startup.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 set -e
 
-echo "== Instalando dependencias =="
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+echo "==> Entrando a /home/site/wwwroot"
+cd /home/site/wwwroot
 
-echo "== Migraciones =="
+echo "==> Mostrando archivos"
+ls -la
+
+# Si tu manage.py NO está en /home/site/wwwroot, descomenta y ajusta:
+# cd hojadevida
+
+echo "==> Instalando dependencias"
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+echo "==> Migraciones"
 python manage.py migrate --noinput
 
-echo "== Static =="
+echo "==> Collectstatic"
 python manage.py collectstatic --noinput
 
-echo "== Arrancando Gunicorn =="
+echo "==> Arrancando Gunicorn"
 gunicorn hojadevida.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 2 --timeout 120
