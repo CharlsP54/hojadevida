488946d70804ec5e657a99524c49d79a74431749
CharlsP54 <carlosponce54104@hotmail.com>
2026-01-05 21:53:23 -0500

    3164

--- startup.sh
+++ startup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+echo "==> Collect static"
+python manage.py collectstatic --noinput
+
 echo "==> Start gunicorn"
 exec gunicorn hojadevida.wsgi:application \
   --bind 0.0.0.0:${PORT:-8000} \
